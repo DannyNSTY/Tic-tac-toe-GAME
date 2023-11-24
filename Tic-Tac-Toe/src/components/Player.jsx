cf3386c088fb03189d1dc4e78682d928a147195e
Daniel Mora Gonzalez <94225356+DannyNSTY@users.noreply.github.com>
2023-11-24 16:48:03 +0100

    Add files via upload

---
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
+
+  return (
+    <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
+    className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </motion.li>
+  );
+}
